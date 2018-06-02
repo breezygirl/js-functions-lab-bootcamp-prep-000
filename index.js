48beb270fedb3054d02922efcb8f2b86c20416c1
Beerscheba Chervil <designergo@aol.com>
2018-06-02 20:47:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,1 @@
 // write your code below!
-var happyHolidays = "Happy holiday!"; {
-console.log(happyHolidays);
-   
-}
-
-
-function happyHolidaysTo (name)
-name === Shauna{
-  return `Happy ${ happy holiday}, ${name}!`;
-}
-
-function happyHolidayTO(easter){
-  return `Happy${holidat}. ${name}!`;
-}
-function holidayCountdown(days)
-days = 200{
-return
-`it's ${days} days until ${holday}!`;}
